c6de522681500a2c23ef7b0afd761abb2306b3bb
ShaniceTang <110757597+ShaniceTang@users.noreply.github.com>
2023-08-02 13:29:21 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,26 +13,9 @@ const app = express()
 app.use(express.json())
 
 //cors
-//const allowedOrigins = ['https://frontend-modstop.onrender.com/', "http://localhost:3000/"];
-const isProduction = process.env.NODE_ENV === 'production';
-
-const allowedOrigins = isProduction ? ['https://frontend-modstop.onrender.com/'] : true;
-
-
-const corsOptions = {
-  origin: allowedOrigins,
-//   (origin, callback) => {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: ["http://localhost:3000", "https://modstop-frontend.onrender.com"]
+}));
 
 //will fire everytime a new request comes in
 app.use((req, res, next)=>{
@@ -55,5 +38,3 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error)=>{
         console.log(error)
     })
-
-
